647de1342983ae89af3c65959b2fe8bb71b024b5
marmilosev <mmilosevic@ffos.hr>
2022-04-09 23:43:44 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,1 @@
-$(document).ready(function() {
-      
-    let button = document.querySelectorAll("button");
-
-    $(".arrowLeft").on("click", () =>{
-        $(".row1 :first-child").remove().appendTo(".row1");
-        $(".row2 :first-child").remove().appendTo(".row2");
-        button.disabled = true;
-      });
-    $(".arrowRight").on("click", () =>{
-        $(".row1 :last-child").remove().prependTo(".row1");
-        $(".row2 :last-child").remove().prependTo(".row2");
-        button.disabled = true;
-      }); 
-
- });
+$(document).ready(function(){let o=$("button");$(".arrowLeftBlue").on("click",()=>{$(".row1 :first-child").remove().appendTo(".row1"),$(".row2 :first-child").remove().appendTo(".row2"),o.disabled=!0}),$(".arrowRightBlue").on("click",()=>{$(".row1 :last-child").remove().prependTo(".row1"),$(".row2 :last-child").remove().prependTo(".row2"),o.disabled=!0})});
