b12814ea76cc9b6b29547378e9a1f4fdd28e0e5b
marmilosev <mmilosevic@ffos.hr>
2022-04-13 01:21:03 +0200

    css, js and html updated

--- js/compiled/js/script.js
+++ js/compiled/js/script.js
@@ -1,17 +1,11 @@
-"use strict";
+"use strict";$(document).ready(function(){
+    var o=document.querySelectorAll("button");
+    $(".arrowLeftBlue").on("click",function(){
+        $(".row1 :first-child").remove().appendTo(".row1").addClass("flash"),
+        $(".row2 :first-child").remove().appendTo(".row2").addClass("flash"),
+        o.disabled=!0}),
+        $(".arrowRightBlue").on("click",function(){
+            $(".row1 :last-child").remove().prependTo(".row1").addClass("slide"),
+            $(".row2 :last-child").remove().prependTo(".row2"),
+            o.disabled=!0})});
 
-$(document).ready(function () {
-
-    var button = document.querySelectorAll("button");
-
-    $(".arrowLeft").on("click", function () {
-        $(".row1 :first-child").remove().appendTo(".row1");
-        $(".row2 :first-child").remove().appendTo(".row2");
-        button.disabled = true;
-    });
-    $(".arrowRight").on("click", function () {
-        $(".row1 :last-child").remove().prependTo(".row1");
-        $(".row2 :last-child").remove().prependTo(".row2");
-        button.disabled = true;
-    });
-});
