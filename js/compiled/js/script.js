77c8023ba3f5038e102ad71a002fbfe7be28385f
marmilosev <mmilosevic@ffos.hr>
2022-04-14 08:38:31 +0200

    style and script minified

--- js/compiled/js/script.js
+++ js/compiled/js/script.js
@@ -1,11 +1,1 @@
-"use strict";$(document).ready(function(){
-    var o=document.querySelectorAll("button");
-    $(".arrowLeftBlue").on("click",function(){
-        $(".row1 :first-child").remove().appendTo(".row1").addClass("flash"),
-        $(".row2 :first-child").remove().appendTo(".row2").addClass("flash"),
-        o.disabled=!0}),
-        $(".arrowRightBlue").on("click",function(){
-            $(".row1 :last-child").remove().prependTo(".row1").addClass("slide"),
-            $(".row2 :last-child").remove().prependTo(".row2"),
-            o.disabled=!0})});
-
+"use strict";$(document).ready(function(){let e=$("button");$(".arrowLeftBlue").on("click",function(){$(".row1 :first-child").remove().appendTo(".row1").addClass("flash"),$(".row2 :first-child").remove().appendTo(".row2").addClass("flash"),e.disabled=!0}),$(".arrowRightBlue").on("click",function(){$(".row1 :last-child").remove().prependTo(".row1").removeClass("flash"),$(".row2 :last-child").remove().prependTo(".row2").removeClass("flash"),e.disabled=!0})});
